e855dbbd0a8bef96a54dd66f8d90b622b71431f4
Pedro Lobo <43168878+LoboRep@users.noreply.github.com>
2019-06-04 20:11:09 +0100

    Update calculate_decimation.m

--- calculate_decimation.m
+++ calculate_decimation.m
@@ -1,12 +1,13 @@
 function signal_out=calculate_decimation(signal_in,fs,nfs)
-fcutpb=nfs-5;
-fcutsb=nfs+5;
+fcutpb=nfs/2-5;
+fcutsb=nfs/2+5;
 ripple_pb=(1.01-0.9)/2;
 ripple_sb=(1.01-0.9)/2;
 HB_filter=0.001;
-order=calculatesfir(fcutsb,fcutpb,ripple_pb,ripple_sb,HB_filter,fs);
-ohcut=1/(fs/nfs);
-FIRL_Coeff = fir1(order, ohcut, 'low');
+FIRL_Coeff=calculatesfir(fcutsb,fcutpb,ripple_pb,ripple_sb,HB_filter,fs,'low');%calculates a low pass filter
+figure(2);
+freqz(FIRL_Coeff,1);
+title('Decimation filter');
 signal_ = filter(FIRL_Coeff,1,signal_in);
 factor=(fs/nfs);
 lengthy=length(signal_in);
